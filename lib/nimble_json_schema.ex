9c8238d61bd10841d82ba6e994b6329f57cf40c2
Christopher Grainger <chris@amplified.ai>
2025-04-01 15:43:24 +0200

    Initial commit :tada:

---
+++ lib/nimble_json_schema.ex
@@ -0,0 +1,18 @@
+defmodule NimbleJsonSchema do
+  @moduledoc """
+  Documentation for `NimbleJsonSchema`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NimbleJsonSchema.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nimble_json_schema_test.exs
@@ -0,0 +1,8 @@
+defmodule NimbleJsonSchemaTest do
+  use ExUnit.Case
+  doctest NimbleJsonSchema
+
+  test "greets the world" do
+    assert NimbleJsonSchema.hello() == :world
+  end
+end
